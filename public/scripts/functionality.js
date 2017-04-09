fc8a5abd9be748d5ec8c8e43949d37a3ab0a29d7
Maria Petre <petremariaaa@gmail.com>
2017-04-09 20:57:29 +0300

    UI skeleton demo

---
+++ public/scripts/functionality.js
@@ -0,0 +1,12 @@
+/*$('#load-login').load("login.html", function() {
+  alert( "Load was performed." );
+});*/
+
+//login
+function login_info() {
+	var username = $('#username').val();
+	var password = $('#password').val();
+	console.log(username,password);
+}
+
+//end Login

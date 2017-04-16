024c58f129b7f0d84c4eb4321d0a9de56fbe9731
Maria Petre <petremariaaa@gmail.com>
2017-04-16 20:54:57 +0300

    Resolved issue: Dropdown Menu

--- public/scripts/functionality.js
+++ public/scripts/functionality.js
@@ -1,5 +1,4 @@
 //Variabile
-var poll = [];
 //login
 function login_info() {
 	var username = $('#username').val();
@@ -20,23 +19,27 @@ function filter_by_county(id) {
     	alert( "error" );
   	});
 }
-//poll = data;
+var poll = data;
 
 //dropdown
 function open_menu() {
 	$(this).parents().addClass('open');
 }
-/*
+
 function filter_by_category(id) {
-	for (var i = 0; i < poll.length; ++i) {
-		if (poll[i].category === id)
-			//TODO
-	}
 	
-}*/
+}
 
+function create_polls_list() {
+	var html = '<div>';
+	for (var i = 0; i < poll.length; ++i)
+		if (poll[i].category === id) {
+			html += '<h2>' + data[i].title + '</h2>';
+			html += '<a href="/votarea/'+ data[i].id + '">';
+			html += '<p>' + data[i].description + '</p>';
+			html += '</a>';
+		}
+	html += '</div>';
+	return html;
+}
 //poll
-/*function get_poll() {
-	
-
-}*/
